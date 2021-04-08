a04f57f902b9430b65c67cdec9aab50b140d5437
EmmaSalvan <emma.salvan@etud.univ-jfc.fr>
2021-04-08 22:06:13 +0200

    Envoie au serveur le pseudo et actions du joueur

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -17,48 +17,71 @@ export class AppComponent {
   pallier: Pallier = new Pallier();
   server: string;
   qtmulti: number = 1;
-  badgeManagers : number = 0;
-  showManagers : boolean = false;
+  badgeManagers: number = 0;
+  showManagers: boolean = false;
   managerDispo: boolean = false;
+  username : string = '';
 
   constructor(private service: RestserviceService, private snackBar: MatSnackBar) {
-    this.server = service.getServer(); 
+    this.createUsername();
+    this.server = service.getServer();
     service.getWorld().then(
       world => {
         this.world = world;
       });
-  } 
+  }
+
   ngOnInit(): void {
-    setInterval(() =>{
+    setInterval(() => {
       this.badgeUpgrade();
     }, 1000);
   }
 
-  popMessage(message : string) : void {
-    this.snackBar.open(message, "", { duration : 2000 })
+  // On crée le pseudo du joueur s'il n'est pas spécifié et on le transmet au serveur 
+  createUsername(): void {
+    this.username = localStorage["username"];
+    if (this.username == ''){
+      let nombreAlea = Math.floor(Math.random() * 10000);
+      this.username = 'Joueur' + nombreAlea;
+      localStorage.setItem("username", this.username);
+    }
+    this.service.setUser(this.username);
   }
 
-  badgeUpgrade():void{
+  // On notifie les changements de pseudo du joueur au serveur
+  onUsernameChanged(): void {
+    localStorage.setItem("username", this.username);
+    this.service.setUser(this.username);
+  }
+
+  // On ouvre une fenêtre pop-up d'une durée de 2 secondes
+  popMessage(message: string): void {
+    this.snackBar.open(message, "", { duration: 2000 })
+  }
+
+  // On modifie le nombre de bagde (nombre de managers achetables)
+  badgeUpgrade(): void {
     this.managerDispo = false;
     this.world.managers.pallier.forEach(element => {
       if (!this.managerDispo) {
         if (this.world.money > element.seuil && !element.unlocked) {
-          this.badgeManagers =+ 1;
-          this.managerDispo = true; 
-        }  
+          this.badgeManagers = + 1;
+          this.managerDispo = true;
+        }
+      }
+    })
   }
-})
-}
 
-
-  onProductionDone(p : Product){
+  // On met a jour le score et l'argent du monde après la production d'un produit
+  onProductionDone(p: Product) {
     this.world.money = this.world.money + p.revenu;
     this.world.score = this.world.score + p.revenu;
+    this.service.putProduit(p);
   }
 
-// Changement de la valeur du bouton Buy pour l'achat de produits (soit 1 soit 10 soit 100 soit max)
-  changeQuantity(){
-     switch (this.qtmulti) {
+  // On change la valeur du bouton Buy pour l'achat de produits (soit 1 soit 10 soit 100 soit max)
+  changeQuantity() {
+    switch (this.qtmulti) {
       case 1:
         this.qtmulti = 10
         break;
@@ -70,28 +93,32 @@ export class AppComponent {
         break;
       default:
         this.qtmulti = 1
-    } 
-}
-
-onBuy(coutBuy : number){
-  this.world.money -= coutBuy;
-  this.world.score -= coutBuy;  
-}
-pageManagers(){
-  this.showManagers = true;
-}
-
-hireManager(manager : Pallier){
-  if(this.world.money >= manager.seuil){
-    this.world.managers.pallier[this.world.managers.pallier.indexOf(manager)].unlocked = true;
-    this.world.products.product.forEach(element => {
-      if (manager.idcible == element.id) {
-        this.world.products.product[this.world.products.product.indexOf(element)].managerUnlocked = true;
-      }
-    });
-    this.world.money = this.world.money - manager.seuil;
-    this.popMessage(manager.name +" acheté!");
+    }
   }
-}
+
+  // On met a jour l'argent du monde après l'achat d'un produit
+  onBuy(coutBuy: number) {
+    this.world.money -= coutBuy;
+  }
+
+  // On affiche la page des managers
+  pageManagers() {
+    this.showManagers = true;
+  }
+
+  // On achète un manager 
+  hireManager(manager: Pallier) {
+    if (this.world.money >= manager.seuil) {
+      this.world.managers.pallier[this.world.managers.pallier.indexOf(manager)].unlocked = true;
+      this.world.products.product.forEach(element => {
+        if (manager.idcible == element.id) {
+          this.world.products.product[this.world.products.product.indexOf(element)].managerUnlocked = true;
+        }
+      });
+      this.world.money = this.world.money - manager.seuil;
+      this.popMessage(manager.name + " acheté!");
+      this.service.putManager(manager);
+    }
+  }
 }
 
