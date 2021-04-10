6a23cd7d6d73b17df05eb40e1eaa9bc081998b97
EmmaSalvan <emma.salvan@etud.univ-jfc.fr>
2021-04-10 20:31:48 +0200

    Fonctionnement avec le serveur

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -22,15 +22,17 @@ export class AppComponent {
   pallier: Pallier = new Pallier();
   server: string;
   qtmulti: number = 1;
+  qtmin: number = 0;
+
   badgeManagers: number = 0;
   badgeUpgrades: number = 0;
+  badgeAngels: number = 0;
+
   showManagers: boolean = false;
   showUpgrades: boolean = false;
   showInvestor: boolean = false;
   showAngels: boolean = false;
   showUnlocks: boolean = false;
-  qtmax: number = 0;
-  qtmin: number = 0;
 
   username: string = '';
 
@@ -46,6 +48,7 @@ export class AppComponent {
   ngOnInit(): void {
     setInterval(() => {
       this.allUnlocks();
+      console.log(this.world.money);
     }, 1000);
   }
 
@@ -76,12 +79,15 @@ export class AppComponent {
 
   // On met a jour le score et l'argent du monde après la production d'un produit
   onProductionDone(p: Product) {
-    this.world.money = this.world.money + (p.revenu * p.quantite) * (1 + (this.world.activeangels * this.world.angelbonus / 100));
-    this.world.score = this.world.score + (p.revenu * p.quantite) * (1 + (this.world.activeangels * this.world.angelbonus / 100));
-    this.world.totalangels = Math.round(this.world.totalangels + (150 * Math.sqrt(this.world.score / Math.pow(10, 15))));
-    //this.service.putProduit(p);
-    this.badgeManagersDispo();
-    this.badgeUpgradesDispo();
+    if (p.timeleft == 0) {
+      this.world.money = this.world.money + (p.revenu * p.quantite) * (1 + (this.world.activeangels * this.world.angelbonus / 100));
+      this.world.score = this.world.score + (p.revenu * p.quantite) * (1 + (this.world.activeangels * this.world.angelbonus / 100));
+      this.world.totalangels = Math.round(this.world.totalangels + (150 * Math.sqrt(this.world.score / Math.pow(10, 15))));
+
+      this.badgeManagersDispo();
+      this.badgeUpgradesDispo();
+      this.badgeAngelsDispo();
+    }
   }
 
   // On change la valeur du bouton Buy pour l'achat de produits (soit 1 soit 10 soit 100 soit max)
@@ -99,24 +105,24 @@ export class AppComponent {
       default:
         this.qtmulti = 1
     }
-    this.qtmax = this.qtmulti;
-    console.log(this.qtmax);
   }
 
   // On met a jour l'argent du monde après l'achat d'un produit
   onBuy(coutBuy: number) {
     this.world.money -= coutBuy;
+
     this.badgeManagersDispo();
     this.badgeUpgradesDispo();
+    this.badgeAngelsDispo();
   }
 
   // On modifie le nombre de bagdes (nombre de managers achetables)
   badgeManagersDispo(): void {
     this.badgeManagers = 0;
     this.world.managers.pallier.forEach(element => {
-        if (this.world.money > element.seuil && element.unlocked == false) {
-          this.badgeManagers += 1;
-        }
+      if (this.world.money > element.seuil && element.unlocked == false) {
+        this.badgeManagers += 1;
+      }
     });
   }
 
@@ -124,8 +130,18 @@ export class AppComponent {
   badgeUpgradesDispo(): void {
     this.badgeUpgrades = 0;
     this.world.upgrades.pallier.forEach(element => {
-        if (this.world.money > element.seuil && element.unlocked == false) {
-          this.badgeUpgrades += 1;
+      if (this.world.money > element.seuil && element.unlocked == false) {
+        this.badgeUpgrades += 1;
+      }
+    });
+  }
+
+  // On modifie le nombre de bagdes (nombre d'angels achetables)
+  badgeAngelsDispo(): void {
+    this.badgeAngels = 0;
+    this.world.angelupgrades.pallier.forEach(element => {
+      if (this.world.activeangels > element.seuil && element.unlocked == false) {
+        this.badgeAngels += 1;
       }
     });
   }
@@ -142,51 +158,50 @@ export class AppComponent {
       this.world.money = this.world.money - manager.seuil;
       this.popMessage(manager.name + " acheté!");
       this.service.putManager(manager);
+
       this.badgeManagersDispo();
       this.badgeUpgradesDispo();
+      this.badgeAngelsDispo();
     }
   }
 
-  // On achète un Cash Upgrade
+  // On achète un cash upgrade
   buyUpgrade(upgrade: Pallier) {
     if (this.world.money >= upgrade.seuil) {
       this.world.upgrades.pallier[this.world.upgrades.pallier.indexOf(upgrade)].unlocked = true;
-      this.world.products.product.forEach(element => {
-        if (upgrade.idcible == element.id) {
-          element.revenu = element.revenu * upgrade.ratio;
-        }
-      });
+      this.productsComponent.forEach(produit => produit.calcUpgrade(upgrade));
       this.world.money = this.world.money - upgrade.seuil;
-    
       this.popMessage(upgrade.name + " acheté!");
+      this.service.putUpgrade(upgrade);
+
       this.badgeManagersDispo();
       this.badgeUpgradesDispo();
-      this.service.putUpgrade(upgrade);
+      this.badgeAngelsDispo();
     }
   }
 
-//On va voir si tout les produits vérifient la condition pour appliquer un allupdate
-  allUnlocks(){
+  // On va voir si tout les produits vérifient la condition pour appliquer un allupdate
+  allUnlocks() {
     this.qtmin = Math.min(...this.world.products.product.map(element => element.quantite))
-    console.log(this.qtmin);
     this.world.allunlocks.pallier.map(element => {
       if (!element.unlocked && this.qtmin >= element.seuil) {
         this.world.allunlocks.pallier[this.world.allunlocks.pallier.indexOf(element)].unlocked = true;
         this.productsComponent.forEach(produit => produit.calcUpgrade(element))
         this.popMessage(element.name + " appliqué sur tous les produits!");
+        this.service.putAllUnlocks(element);
       }
     })
   }
 
-//On supprime le monde et on met à jour la page
-  claimAngel(): void{
-   this.service.deleteWorld();
-   window.location.reload();
+  // On supprime le monde et on met à jour la page
+  claimAngel(): void {
+    this.service.deleteWorld();
+    window.location.reload();
   }
 
-//On dépense des anges
-buyAngel(){
+  // On achète un ange
+  buyAngel() {
 
-}
+  }
 }
 
--- src/app/product/product.component.ts
+++ src/app/product/product.component.ts
@@ -16,141 +16,143 @@ export class ProductComponent implements OnInit {
   progressbarvalue: number = 0;
   lastupdate: number = 0;
   coutBuy: number = 0;
+  qtmax: number = 0;
 
-// On récupére la valeur de qtmulti
-_qtmulti: number = 0;
-@Input()
-set qtmulti(value: number) {
-  this._qtmulti = value;
-  if (this._qtmulti == 1000) {
-    this._qtmulti = this.calcMaxCanBuy();
+  // On récupére la valeur de qtmulti
+  _qtmulti: number = 0;
+  @Input()
+  set qtmulti(value: number) {
+    this._qtmulti = value;
+    if (this._qtmulti == 1000) {
+      this._qtmulti = this.qtmax;
+    }
   }
-}
 
-_qtmax: number = 0;
-@Input()
-set qtmax(value: number) {
-  this._qtmax = value;
-}
-
-
-// On récupere le produit du monde
-@Input()
-set prod(value: Product) {
-  this.product = value;
-  //if (this.product && this.product.timeleft > 0) {
-    //this.lastupdate = Date.now();
-    //let progress = (this.product.vitesse - this.product.timeleft) / this.product.vitesse;
+  // On récupere le produit du monde
+  @Input()
+  set prod(value: Product) {
+    this.product = value;
+    if (this.product && this.product.timeleft > 0) {
+    this.lastupdate = Date.now();
+    let progress = (this.product.vitesse - this.product.timeleft) / this.product.vitesse;
     //this.progressbar.set(progress);
     //this.progressbar.animate(1, { duration: this.product.timeleft });
-  //}
-}
-
-// On récupére l'argent du monde
-_money: number = 0;
-@Input()
-set money(value: number) {
-  this._money = value;
-}
-
-
-// On envoie le produit qui est produit
-@Output() notifyProduction: EventEmitter < Product > = new EventEmitter<Product>();
-
-// On envoie le coût du produit acheté
-@Output() onBuy: EventEmitter < number > = new EventEmitter<number>();
-
-constructor(private service: RestserviceService, private snackBar: MatSnackBar) {
-  this.server = service.getServer();
-}
-ngOnInit(): void {
-  setInterval(() => {
-  this.calcScore();
-  if (this._qtmulti == 1000) {
-    this._qtmax = this.calcMaxCanBuy();
-    console.log(this.qtmax);
-  }
-}, 1000);
-  }
-
-// On commence la fabrication d'un produit s'il n'est pas déjà en cours de fabrication
-startFabrication() {
-  //console.log(this.product.vitesse);
-  if (this.estFabrique == false && this.product.quantite > 0) {
-    this.estFabrique = true;
-    this.lastupdate = Date.now();
-    this.product.timeleft = this.product.vitesse;
-  }
-}
-
-// Pour un produit et en fonction du temps écoulé depuis la dernière fois, on décrémente le temps restant de production du produit.
-// Si ce temps devient négatif ou nul, on ajoute l’argent généré au score et efface la barre de production.
-calcScore() {
-  // Si le manager est acheté, on lance la production
-  if (this.product.timeleft == 0 && this.product.managerUnlocked) {
-    this.startFabrication();
-  }
-  // Le produit est en production, on calcule le nouveau timeleft
-  if (this.product.timeleft != 0) {
-    this.product.timeleft = this.product.timeleft - (Date.now() - this.lastupdate);
-    this.lastupdate = Date.now();
-    // Le temps est écoulé, on ajoute le produit et remet sa barre et son temps à 0
-    if (this.product.timeleft <= 0) {
-      this.product.timeleft = 0;
-      this.progressbarvalue = 0;
-      this.notifyProduction.emit(this.product);
-      //this.service.putProduit(this.product);
-      this.estFabrique = false;
     }
-    // On calcule la nouvelle valeur de la barre de progression
+  }
+
+  // On récupére l'argent du monde
+  _money: number = 0;
+  @Input()
+  set money(value: number) {
+    this._money = value;
+  }
+
+
+  // On envoie le produit qui est produit
+  @Output() notifyProduction: EventEmitter<Product> = new EventEmitter<Product>();
+
+  // On envoie le coût du produit acheté
+  @Output() onBuy: EventEmitter<number> = new EventEmitter<number>();
+
+  constructor(private service: RestserviceService, private snackBar: MatSnackBar) {
+    this.server = service.getServer();
+  }
+
+  ngOnInit(): void {
+    setInterval(() => {
+      this.calcScore();
+      this.qtmax = this.calcMaxCanBuy();
+      if (this._qtmulti == 1000) {
+        this._qtmulti = this.qtmax;
+      }
+    }, 1000);
+  }
+
+  // On commence la fabrication d'un produit (s'il n'est pas déjà en cours de fabrication)
+  startFabrication() {
+    if (this.product.quantite > 0) {
+      if (this.estFabrique == false) {
+        this.estFabrique = true;
+        this.product.timeleft = this.product.vitesse;
+        this.lastupdate = Date.now();
+        this.service.putProduit(this.product);
+      }
+    }
     else {
-      this.progressbarvalue = ((this.product.vitesse - this.product.timeleft) / this.product.vitesse) * 100;
+      this.popMessage("Tu dois d'abord acheter le produit " + this.product.name + " !")
     }
   }
-}
 
-// On calcule le maximum de produit que l'on peut acheter avec l'argent
-calcMaxCanBuy(): number {
-  let qtmax: number = 0;
-  if (this.product.cout * this.product.croissance <= this._money) {
-    let qt = (Math.log((this.product.cout - (this._money * (1 - this.product.croissance))) / this.product.cout)) / Math.log(this.product.croissance);
-    qtmax = Math.floor(qt);
-    if (qtmax < 0) {
-      qtmax = 0;
+  // Pour un produit et en fonction du temps écoulé depuis la dernière fois, on décrémente le temps restant de production du produit.
+  // Si ce temps devient négatif ou nul, on ajoute l’argent généré au score et efface la barre de production.
+  calcScore() {
+    // Si le manager est acheté, on lance la production
+    if (this.product.timeleft == 0 && this.product.managerUnlocked) {
+      this.startFabrication();
+    }
+    // Le produit est en production, on calcule le nouveau timeleft
+    console.log(this.product.name, this.product.timeleft);
+    if (this.product.timeleft != 0) {
+      this.product.timeleft = this.product.timeleft - (Date.now() - this.lastupdate);
+      this.lastupdate = Date.now();
+      // Le temps est écoulé, on ajoute le produit et remet sa barre et son temps à 0
+      if (this.product.timeleft <= 0) {
+        this.product.timeleft = 0;
+        this.progressbarvalue = 0;
+        this.estFabrique = false;
+        this.notifyProduction.emit(this.product);
+      }
+      // On calcule la nouvelle valeur de la barre de progression
+      else {
+        this.progressbarvalue = ((this.product.vitesse - this.product.timeleft) / this.product.vitesse) * 100;
+      }
     }
   }
-  return qtmax;
-}
 
-// On achète le produit 
-buyProduct() {
-  this.coutBuy = this._qtmulti * this.product.cout;
-  this.product.cout = Math.round(this.product.cout * this.product.croissance ** this._qtmulti * 100) / 100;
-  this.product.quantite = this.product.quantite + this._qtmulti;
-  this.onBuy.emit(this.coutBuy);
-
-
-  // Les unlocks 
-  this.product.palliers.pallier.forEach(element =>{
-     if (this.product.quantite > element.seuil && element.unlocked == false){
-      this.calcUpgrade(element); 
-      this.product.palliers.pallier[this.product.palliers.pallier.indexOf(element)].unlocked = true;
-      this.popMessage(element.name + " obtenu !");
-     }
-  })
-}
-
-calcUpgrade(pallier : Pallier){
-  if (pallier.typeratio == "vitesse"){
-    this.product.vitesse = this.product.vitesse / pallier.ratio;
+  // On calcule le maximum de produit que l'on peut acheter avec l'argent
+  calcMaxCanBuy(): number {
+    let qtmax: number = 0;
+    if (this.product.cout * this.product.croissance <= this._money) {
+      let qt = (Math.log((this.product.cout - (this._money * (1 - this.product.croissance))) / this.product.cout)) / Math.log(this.product.croissance);
+      qtmax = Math.floor(qt);
+      if (qtmax < 0) {
+        qtmax = 0;
+      }
+    }
+    return qtmax;
   }
-  if (pallier.typeratio == "gain"){
-    this.product.revenu = this.product.revenu * pallier.ratio;
-  }
-}
 
-popMessage(message: string): void {
-  this.snackBar.open(message, "", { duration: 2000 })
-}
+  // On achète le produit 
+  buyProduct() {
+    this.coutBuy = this._qtmulti * this.product.cout;
+    this.product.cout = Math.round(this.product.cout * this.product.croissance ** this._qtmulti * 100) / 100;
+    this.product.quantite = this.product.quantite + this._qtmulti;
+    this.onBuy.emit(this.coutBuy);
+    this.service.putProduit(this.product);
+
+    // Les unlocks 
+    this.product.palliers.pallier.forEach(element => {
+      if (this.product.quantite > element.seuil && element.unlocked == false) {
+        this.calcUpgrade(element);
+        this.product.palliers.pallier[this.product.palliers.pallier.indexOf(element)].unlocked = true;
+        this.popMessage(element.name + " obtenu !");
+      }
+    })
+  }
+
+  // On calcule l'effet de l'upgrade en fonction de son type
+  calcUpgrade(pallier: Pallier) {
+    if (pallier.typeratio == "vitesse") {
+      this.product.vitesse = this.product.vitesse / pallier.ratio;
+    }
+    if (pallier.typeratio == "gain") {
+      this.product.revenu = this.product.revenu * pallier.ratio;
+    }
+  }
+
+  // On affiche un message d'une durée de 2 secondes
+  popMessage(message: string): void {
+    this.snackBar.open(message, "", { duration: 2000 })
+  }
 
 }
